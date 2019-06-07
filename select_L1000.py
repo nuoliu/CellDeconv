562960f71e19c36814ac9e3e3b91a675a7957430
Nuo Liu <ivliu@Nuos-MacBook-Pro.local>
2019-06-07 15:56:33 -0500

    explored variation and stuff

--- select_L1000.py
+++ select_L1000.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import regex
 def constructSet():
     L1000=set()
     f = open("data/L1000.txt", "r")
@@ -15,8 +16,12 @@ def selectExpression():
     L1000=constructSet()
     data=pd.read_excel("data/Wilms_gene_tpms_all_samples.xlsx", sheet_name="gene_tpms_all_samples")
     selected=data.loc[data['geneSymbol'].isin(L1000)]
-    with pd.ExcelWriter('data/L1000_rnaseq.xlsx') as writer:
-        selected.to_excel(writer, index=False)
+    tumor_col=[col for col in selected if "118" in col or "163" in col or "565" in col]
+    to_keep=["target_id","biotype","geneSymbol"]
+    to_keep.extend(tumor_col)
+    tumor_=selected[to_keep]
+    with pd.ExcelWriter('data/L1000_tumor_rnaseq.xlsx') as writer:
+        tumor_.to_excel(writer, index=False)
 
 if __name__=="__main__":
         selectExpression()
