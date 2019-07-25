e416fc4989b2d322aee46cb65a2966a03b84b344
Nuo Liu <ivliu@Nuos-MacBook-Pro.local>
2019-07-25 11:49:12 -0500

    commented

--- shell scripts/wilms.sh
+++ shell scripts/wilms.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-
+#switch to your working directory!
 cd "/home/nl2/SMART_2019/CellDeconv"
 EXPATH="/home/nl2/SMART_2019/CellDeconv/data/wilms"
 OUT="/home/nl2/SMART_2019/CellDeconv/wilms_experiment"
@@ -14,7 +14,7 @@ for tumor in "tumor_118" "tumor_163" "tumor_565"
 
             # echo "python lp.py -i $EXPATH/sc_mixture.xlsx -o $EXPATH/ -f True -a $alpha"
             echo "$tumor, $alpha"
-            python lp_2.py -i $EXPATH/${tumor}.xlsx -o $current -f True -a $alpha
+            python ../code/lp.py -i $EXPATH/${tumor}.xlsx -o $current -f True -a $alpha
            
 
             done
